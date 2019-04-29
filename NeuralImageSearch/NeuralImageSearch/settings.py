3895389963741a351df193117724f8e5d250683d
K_Zdor <k.zdor@cvrndteam.com>
2019-04-29 22:10:50 +0300

    [Add] account app.

--- NeuralImageSearch/NeuralImageSearch/settings.py
+++ NeuralImageSearch/NeuralImageSearch/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'WebSearch',
-    'crispy_forms'
+    'crispy_forms',
+    'accounts.apps.AccountsConfig',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
